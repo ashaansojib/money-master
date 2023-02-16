0b2f17dfc62f8d406b7b523302aa5b8f8049ab12
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-02-16 21:46:37 +0600

    complte the money master

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 /*
 Note: kono ekta value ke sum kore onno element a show korar jonne duita kaj korte hy. first sekhane show korabo take dhore ante hbe r je value ta add hobe take inner text hisebe set korte hbe.
 */
-// common get input field value
+
+//  get common input field value
 function inputElementById(inputId) {
     const inputElement = document.getElementById(inputId);
     const getInputValue = inputElement.value;
@@ -9,6 +10,7 @@ function inputElementById(inputId) {
     inputElement.value = '';
     return getInputNumber;
 }
+
 // get the showing id
 function currentElementById(currentElement) {
     const getCurrentElement = document.getElementById(currentElement).innerText;
@@ -21,9 +23,9 @@ function setElementById(currentElement, newTotal) {
     setElement.innerText = newTotal;
 }
 document.getElementById("calculate-btn").addEventListener('click', function () {
+
     // my total cash or income value
     const myTotalBudget = inputElementById("income");
-
     // my food cost
     const myFoodCost = inputElementById("food");
 
@@ -33,13 +35,19 @@ document.getElementById("calculate-btn").addEventListener('click', function () {
     // my clothes cost
     const myClothesCost = inputElementById("clothes");
 
+    if(myTotalBudget < 0 || myFoodCost < 0 || myRentCost < 0 || myClothesCost < 0){
+        return alert("please, provide number!!");
+    }
     // my Total Expense
     const totalExpense = myFoodCost + myRentCost + myClothesCost;
-        // validation if the user cost is over than budget
-        if(myTotalBudget < totalExpense){
-            alert("wrong");
-            return;
-        }
+    if(isNaN(totalExpense)){
+        return alert("input a number please!!");
+    }
+    // validation if the user cost is over than budget
+    if (myTotalBudget < totalExpense) {
+        alert("You not have an enough money!!");
+        return;
+    }
     // set the total expense
     setElementById("total-expense", totalExpense);
 
@@ -53,7 +61,12 @@ document.getElementById("calculate-btn").addEventListener('click', function () {
 document.getElementById("save-btn").addEventListener('click', function () {
     // find the saving value and set into saving, finally set remaining balance
     const mySaving = inputElementById("save");
-
+    if(mySaving < 0){
+        return alert("Input a Positive number");
+    }
+    else if(isNaN(mySaving)){
+        return alert("Your input not a number");
+    }
     // my current total balance and find saving amount
     const totalCurrentBalance = currentElementById("balance");
 
@@ -64,4 +77,4 @@ document.getElementById("save-btn").addEventListener('click', function () {
     setElementById("saving-amount", savingAmount);
     // set remaining balance
     setElementById("remaining-balance", remainingBalance);
-})
+});
